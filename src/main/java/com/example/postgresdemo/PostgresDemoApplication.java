1b2b92b6a209a887fd283628e55308662d4ac910
pactera-testing-wangyuzheng <P0001217@PC-2022>
2024-10-23 19:50:56 +0900

    20241023 wyz reset

--- src/main/java/com/example/postgresdemo/PostgresDemoApplication.java
+++ src/main/java/com/example/postgresdemo/PostgresDemoApplication.java
@@ -8,16 +8,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+
 @SpringBootApplication
 @EnableJpaAuditing
 public class PostgresDemoApplication {
 	public static void main(String[] args) {
 		Date date1 = new Date();
 		SimpleDateFormat formatter =new SimpleDateFormat("yy-mm-dd HH:mm:ss");
+		
+
 		System.out.println("Start:" + formatter.format(date1));
 		SpringApplication.run(PostgresDemoApplication.class, args);
 		Date date2 = new Date();
 		System.out.println("End:" + formatter.format(date2));
 		System.out.println("test");
+
 	}
 }
